0c98424358282d26ee8561e53c636c764cb2f6c7
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-08-10 22:03:44 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "../styles";
+
+const Navbar = () => {
+  return <div>Naveee.bar</div>;
+};
+
+export default Navbar;
