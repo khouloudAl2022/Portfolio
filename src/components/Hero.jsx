533c906005e384f9dc7d6b2c85e7ef16b59d4404
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-08-13 18:53:43 +0100

    feat: Add hero section to homepage
    Added a hero section to the homepage, including a profile image and introduction text. The section uses flex layout for responsiveness and includes a gradient background. Updated the heroHeadText and heroSubText styles to improve readability and maintain consistency across the page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { styles } from "../styles";
+import Computers from "./canvas/Computers";
 
 const Hero = () => {
   return (
@@ -6,8 +7,21 @@ const Hero = () => {
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex  flex-row items-start gap-5`}
       >
-        <div className="flex flex-col justify-center"></div>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#FF00FF]" />
+          <div className="w-1 sm:h-80 h-1 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi , I&apos;m <span className="text-[#FF00FF]">Khouloud</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop user <br className="sm:block hidden" /> interfaces and web
+            applications
+          </p>
+        </div>
       </div>
+      <Computers />
     </section>
   );
 };
