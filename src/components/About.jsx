68420d5817a6675b7094d821c6839bfb9ef76cfd
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-08-15 14:11:40 +0100

    Add div contain map of services for each service object a ServiceCard component is rendered

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,9 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motions";
 
+const ServiceCard = ({ index, title, icon }) => {
+  return <p>{title}</p>;
+};
 const About = () => {
   return (
     <>
@@ -11,12 +14,20 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
-      <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
         distinctio ut, at, voluptas beatae aut ab deserunt officia tempore
         voluptates magni atque necessitatibus nam laborum! Corrupti architecto
         numquam ab.
       </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   );
 };
