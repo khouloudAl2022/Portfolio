8b8789464a12a7dd4c86c999c14d7e0020fbbd46
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-08-15 21:05:36 +0100

    Added ExperienceCard component to display work experience details with customized styles and icons.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,7 @@
-import { VerticalTimeline } from "react-vertical-timeline-component";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
 import { motion } from "framer-motion";
 import "react-vertical-timeline-component/style.min.css";
 
@@ -7,9 +10,27 @@ import { SectionWrapper } from "../hoc";
 import { textVariant } from "../utils/motions";
 import { styles } from "../styles";
 
-const ExperienceCard=({experience})=>{
-
-}
+const ExperienceCard = ({ experience }) => (
+  <VerticalTimelineElement
+    contentStyle={{ background: "#1d1836", color: "#fff" }}
+    contentArrowStyle={{ borderRight: "7px solid #232631" }}
+    date={experience.date}
+    iconStyle={{ background: experience.iconBg }}
+    icon={
+      <div>
+        <img
+          src={experience.icon}
+          alt={experience.company_name}
+          className="w-[60%] h-[60%] object-container"
+        />
+      </div>
+    }
+  >
+    <div>
+      <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+    </div>
+  </VerticalTimelineElement>
+);
 
 const Experience = () => {
   return (
