1719e6261fd5a8f7b647f2fd4e6501cdf155016e
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-08-15 20:21:13 +0100

    Added Higher-Order Component (HOC) for section wrapping and animations

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motions";
-
+import { SectionWrapper } from "../hoc";
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
@@ -16,7 +16,9 @@ const ServiceCard = ({ index, title, icon }) => {
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
           <img src={icon} alt={title} className="w-16 h-16 object-contain" />
-          <h3 className="text-white text-[20px] font-bolt text-center">{title}</h3>
+          <h3 className="text-white text-[20px] font-bolt text-center">
+            {title}
+          </h3>
         </div>
       </motion.div>
     </Tilt>
@@ -47,4 +49,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
