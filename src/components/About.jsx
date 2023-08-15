5356554b0014c242ddd5465c940d5d6dacc1452b
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-08-15 13:54:02 +0100

    Add some styles for the paragraphe

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,12 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
-      <motion.p variants={fadeIn("", "", 0.1, 1)}></motion.p>
+      <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        distinctio ut, at, voluptas beatae aut ab deserunt officia tempore
+        voluptates magni atque necessitatibus nam laborum! Corrupti architecto
+        numquam ab.
+      </motion.p>
     </>
   );
 };
