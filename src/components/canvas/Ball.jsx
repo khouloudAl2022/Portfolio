c030e5f05957a202e03601d6d72051961e22d825
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-08-16 12:59:19 +0100

    Add a decal to can be adding a texture or image into surface of the mesh and give it position

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -17,7 +17,13 @@ const Ball = (props) => {
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial/>
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal position={[0,0,1]} map={decal} />
       </mesh>
     </Float>
   );
