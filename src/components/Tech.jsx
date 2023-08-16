f6b892c1cc2f9110295ec93a50ac85a55e403018
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-08-16 12:47:17 +0100

    Feat:Add BallCanvas component for rendering a floating ball with a texture image

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,17 @@
+import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">Tech</div>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) => (
+        <div>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
   );
 };
 
