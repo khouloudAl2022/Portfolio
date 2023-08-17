bff1a468b2c2da407764c1bfb514132670cafec8
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-08-17 18:49:07 +0100

    Feat: Added Contact component containing form with label and inputs with styles

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,8 +29,10 @@ import tesla from "./company/tesla.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+import circleImg from "./circle.png";
 
 export {
+  circleImg,
   mylogo2,
   backend,
   creator,
